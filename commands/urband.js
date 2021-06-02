c2f8f2a68a05b07ed123ab2eb2edfe74d3946d37
Luis Bauza <hllywluis2@me.com>
2021-06-01 22:11:36 -0400

    Attempted fix for heroku

--- commands/urband.js
+++ commands/urband.js
@@ -31,8 +31,8 @@ module.exports = {
                         .setTitle(word)
                         .setURL(encodeURI('https://www.urbandictionary.com/define.php?term=' + word))
                         .setAuthor('Urban Dictionary', 'https://apprecs.org/ios/images/app-icons/256/74/584986228.jpg', 'https://www.urbandictionary.com')
-                        .addField('Definition by ' + def[0].author.replace(/\[/g, '').replace(/]/g, '') + ':', def[0].definition >= 1024 ? def[0].definition.replace(/\[/g, '').replace(/]/g, '').substr(0, 1021) + '...' : def[0].definition.replace(/\[/g, '').replace(/]/g, ''))
-                        .addField('Example:', def[0].example >= 1024 ? def[0].example.replace(/\[/g, '').replace(/]/g, '').substr(0, 1021) + '...' : def[0].example.replace(/\[/g, '').replace(/]/g, ''))
+                        .addField('Definition by ' + def[0].author.replace(/\[/g, '').replace(/]/g, '') + ':', def[0].definition >= 1024 ? def[0].definition.replace(/\[/g, '').replace(/]/g, '').substr(0, 1020) + '...' : def[0].definition.replace(/\[/g, '').replace(/]/g, ''))
+                        .addField('Example:', def[0].example >= 1024 ? def[0].example.replace(/\[/g, '').replace(/]/g, '').substr(0, 1020) + '...' : def[0].example.replace(/\[/g, '').replace(/]/g, ''))
                         .addField('Thumbs Up:', def[0].thumbs_up, true)
                         .addField('Thumbs Down:', def[0].thumbs_down, true)
 
